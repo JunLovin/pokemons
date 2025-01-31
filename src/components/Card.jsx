7292623e0e6adc851d61605aaac5c13f5f7ef88a
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-31 12:17:15 -0500

    Delete logic and make shiny button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,59 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import '@styles/Card.css'
 
-function Card() {
-    const [pokemonData, setPokemonData] = useState([])
-    const [loading, isLoading] = useState(true)
-    const [error, setError] = useState(null)
+function Card({ name, id, normalImage, shinyImage }) {
     const [shiny, setShiny] = useState(false)
-
-    const pokemonApi = () => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/`)
-        .then((response) => response.json())
-        .then((response) => {
-            setPokemonData(response)
-            isLoading(false)
-        })
-        .catch((error) => {
-            setError(error)
-            isLoading(false)
-        })
-    }
-
-    useEffect(() => {
-        pokemonApi()
-    }, [])
-
-    if (loading) {
-        return <div>Loading...</div>
-    }
-
-    if (error) {
-        console.error(error)
-    }
 
     const handleShiny = () => {
         setShiny(!shiny)
     }
 
-    console.log(pokemonData.results)
-
     return (
         <>
-        <h1>Pokemon Card List</h1>
         <div className="card-container">
             <div className="card-image-container">
                 <img
-                src={!shiny ? pokemonData[1].sprites.front_default : pokemonData[1].sprites.front_shiny}
-                alt={pokemonData[1].name}
+                src={shiny ? shinyImage : normalImage}
+                alt={name}
                 />
             </div>
             <div className="card-text-container">
-                <h2>Pokemon Name: {pokemonData[1].name}</h2>
-                <p><b>ID</b>: {pokemonData[1].id}</p>
+                <h2>Name: {name}</h2>
+                <p><b>ID</b>: {id}</p>
             </div>
             <div className="card-button-container">
-                <button onClick={handleShiny}>Shiny</button>
+                <button onClick={() => handleShiny()}>Shiny</button>
             </div>
         </div>
         </>
